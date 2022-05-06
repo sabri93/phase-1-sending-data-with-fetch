a2ca33f4f93334b536d1e9ff78a804f9bb6240c7
sabrina <sabrinadjebbar93@gmail.com>
2022-05-06 17:28:14 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Add your code here
+ function submitData(userName,userEmail){
+       
+    const configurationObject ={
+        method: "POST",
+        headers: {
+            "content-Type":'application/json',
+            Accept: "application/json",
+            
+        },
+        body: JSON.stringify({
+         name : userName,
+         email : userEmail
+
+        })
+    }
+    
+   return fetch("http://localhost:3000/users",configurationObject)
+   .then(response=>response.json())  
+   .then(function(object){
+    console.log(object.id)
+      let elem = document.createElement("p")
+     elem.innerHTML = object.id
+   document.body.append(elem)
+  
+
+
+   })
+   .catch(function (error) {
+    console.log(error.message);
+    
+    let el = document.createElement("p")
+   el.innerHTML = error.message
+   document.body.append(el)
+     
+   })
+ 
+
+ }
+
+ 
+ 
+
+
+   
+
+  
+
+
+
